963c07c4111658105d817dbd7291ab9d0b1c6dcc
BuildingIDentity <peter@boxydesign.be>
2025-11-29 18:57:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import json
+from utils.db import init_tables
+from scrapers.immoweb import scrape_immoweb
+
+def load_postcodes():
+    with open("config/postcodes.json", "r") as f:
+        return json.load(f)
+
+def main():
+    print("Initialiseren tabellen...")
+    init_tables()
+    
+    postcodes = load_postcodes()
+
+    # Test: eerst 1 postcode
+    for pc in postcodes[:1]:
+        print(f"Scrapen immoweb {pc}")
+        scrape_immoweb(pc)
+
+    print("Klaar.")
+
+if __name__ == "__main__":
+    main()
 
