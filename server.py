ac6ed51015a110b966e670dd3a2151551b5823bb
BuildingIDentity <peter@boxydesign.be>
2025-11-30 18:09:10 +0100

    Update server.py

--- server.py
+++ server.py
@@ -28,5 +28,7 @@ def start_zimmo():
 
     return jsonify({"status": "ok", "postcodes": postcodes})
 
+# Heroku gebruikt gunicorn, dus lokaal runnen mag wel:
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
