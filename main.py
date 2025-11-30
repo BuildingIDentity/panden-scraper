86e63829a095809da3b1a85c531e22a5d7961df2
BuildingIDentity <peter@boxydesign.be>
2025-11-30 01:33:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,23 @@
 import json
 from utils.db import init_tables
-from scrapers.immoweb import scrape_immoweb
 from scrapers.zimmo import scrape_zimmo
-from scrapers.immoscoop import scrape_immoscoop
-from scrapers.tweedehands import scrape_tweedehands
-from scrapers.immovlam import scrape_immovlam
 
 def load_postcodes():
     with open("config/postcodes.json", "r") as f:
         return json.load(f)
 
 def main():
-        print("Initialiseren tabellen...")
-        init_tables()
+    print("Initialiseren tabellen...")
+    init_tables()
 
-        postcodes = load_postcodes()
+    postcodes = load_postcodes()
 
-        for pc in postcodes[:2]:
-            print(f"Scrapen immoweb {pc}")
-            scrape_immoweb(pc, "koop")
-            scrape_immoweb(pc, "huur")
+    for pc in postcodes[:1]:   # enkel 1 postcode om te testen
+        print(f"Scrapen zimmo {pc}")
+        scrape_zimmo(pc, "koop")
+        scrape_zimmo(pc, "huur")
 
-            print(f"Scrapen zimmo {pc}")
-            scrape_zimmo(pc, "koop")
-            scrape_zimmo(pc, "huur")
-
-            print(f"Scrapen immoscoop {pc}")
-            scrape_immoscoop(pc, "koop")
-            scrape_immoscoop(pc, "huur")
-
-            print(f"Scrapen tweedehands {pc}")
-            scrape_tweedehands(pc, "koop")
-            scrape_tweedehands(pc, "huur")
-
-            print(f"Scrapen immovlam {pc}")
-            scrape_immovlam(pc, "koop")
-            scrape_immovlam(pc, "huur")
-
-        print("Klaar.")
+    print("Klaar.")
 
 if __name__ == "__main__":
     main()
